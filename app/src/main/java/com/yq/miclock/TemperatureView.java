0729630de8a026208577dafcd6637135f725f039
yinqi <251282197.com>
2019-06-03 16:47:08 +0800

    backup

--- app/src/main/java/com/yq/miclock/TemperatureView.java
+++ app/src/main/java/com/yq/miclock/TemperatureView.java
@@ -1,20 +1,29 @@
 package com.yq.miclock;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.graphics.SweepGradient;
+import android.support.annotation.MainThread;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class TemperatureView extends View {
 
-    private Paint bgPaint, dashPaint, linePaint, currentLinePaint;
+    private int colorBegin, colorEnd, dotColor, distance, tempBegin, tempEnd, currentTemp;
+    private float deltaTemp;//两条刻度线之间的温度差值
+    private float scaleRatio = 0.25f;//刻度线长度占控件宽度比例
+    private Paint bgPaint, //渐变色背景画笔
+            dotPaint, //圆点画笔
+            linePaint, currentLinePaint;
     private int centerX, centerY;
     private int ringWidth, dotStartX, padding, mWidth, mHeight;
+    private SweepGradient bgGradient;
 
     public TemperatureView(Context context) {
         this(context, null);
@@ -26,24 +35,43 @@ public class TemperatureView extends View {
 
     public TemperatureView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initAttrs(context, attrs);
         init();
     }
 
+    private void initAttrs(Context context, @Nullable AttributeSet attrs) {
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.TemperatureView);
+        colorBegin = typedArray.getColor(R.styleable.TemperatureView_color_begin, Color.BLUE);
+        colorEnd = typedArray.getColor(R.styleable.TemperatureView_color_end, Color.GREEN);
+        dotColor = typedArray.getColor(R.styleable.TemperatureView_color_dot, Color.GRAY);
+        tempBegin = typedArray.getInt(R.styleable.TemperatureView_tempValue_start, 16);
+        tempEnd = typedArray.getInt(R.styleable.TemperatureView_tempValue_end, 40);
+        scaleRatio = typedArray.getFloat(R.styleable.TemperatureView_scale_ratio, 0.25f);
+        padding = typedArray.getDimensionPixelOffset(R.styleable.TemperatureView_padding, 200);
+        distance = typedArray.getDimensionPixelOffset(R.styleable.TemperatureView_distance, 100);
+        typedArray.recycle();
+        deltaTemp = (float) (tempEnd - tempBegin) / 270;//平分成60格，底部15格不显示刻度
+        currentTemp = tempBegin;
+    }
+
     private void init() {
+
         setBackgroundColor(Color.WHITE);
         bgPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         bgPaint.setStyle(Paint.Style.STROKE);
+        bgGradient = new SweepGradient(centerX, centerY, new int[]{colorBegin, colorEnd}, null);
+        bgPaint.setShader(bgGradient);
 
-        dashPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        dashPaint.setColor(Color.GRAY);
-        dashPaint.setStrokeWidth(2);
+        dotPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        dotPaint.setColor(dotColor);
+        dotPaint.setStrokeWidth(2);
 
         linePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         linePaint.setColor(Color.WHITE);
         linePaint.setStyle(Paint.Style.STROKE);
 
         currentLinePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        currentLinePaint.setColor(Color.YELLOW);
+        currentLinePaint.setShader(bgGradient);
         currentLinePaint.setStyle(Paint.Style.STROKE);
         currentLinePaint.setStrokeWidth(4);
 
@@ -56,46 +84,120 @@ public class TemperatureView extends View {
         mHeight = getMeasuredHeight();
         centerX = getMeasuredWidth() / 2;
         centerY = getMeasuredHeight() / 2;
-        ringWidth = (int) (0.25 * centerX);
-        padding = 40;
-        SweepGradient bgGradient = new SweepGradient(centerX, centerY, new int[]{Color.BLUE, Color.GREEN}, null);
-        bgPaint.setShader(bgGradient);
+        ringWidth = (int) (scaleRatio * centerX);
+
         bgPaint.setStrokeWidth(ringWidth);
         linePaint.setStrokeWidth(ringWidth + 4);
-//        currentLinePaint.setStrokeWidth(ringWidth + 20);
-        dotStartX = getMeasuredWidth() - 40 - ringWidth;
+        dotStartX = getMeasuredWidth() - padding - ringWidth - distance;
+    }
+
+    float downX = 0;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                downX = event.getX();
+                System.out.println(downX);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float currentX = event.getX();
+                float diffX = currentX - downX;
+                System.out.println("diffX =  " + diffX);
+                setCurrentTempByDelta(calcDeltaTemp(diffX));
+                downX = currentX;
+                break;
+//                return true;
+            case MotionEvent.ACTION_UP:
+
+                break;
+        }
+        return true;
+    }
+
+    /**
+     * 计算温度变化值
+     *
+     * @param diff 滑动距离
+     * @return
+     */
+    private float calcDeltaTemp(float diff) {
+        return diff / mWidth * (tempEnd - tempBegin) + 0.5f;
+    }
+
+    private void setCurrentTempByDelta(float deltaTemp) {
+        System.out.println("deltaTemp =  " + deltaTemp);
+        currentTemp += deltaTemp;
+        if (currentTemp < tempBegin)
+            currentTemp = tempBegin;
+        else if (currentTemp > tempEnd)
+            currentTemp = tempEnd;
+        System.out.println(currentTemp);
+        setCurrentTemp(currentTemp);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         drawBackground(canvas);
+
     }
 
+    //画刻度盘
     private void drawBackground(Canvas canvas) {
         RectF rectF = new RectF(0, 0, mWidth, mHeight);
         rectF.inset(ringWidth / 2 + padding, ringWidth / 2 + padding);
 //        RectF rectF = new RectF(ringWidth / 2 + padding, ringWidth / 2 + padding, getWidth() - ringWidth / 2 - padding, getHeight() - ringWidth / 2 - padding);
         canvas.save();
-        canvas.rotate(135, centerX, centerY);
+        canvas.rotate(-225, centerX, centerY);
         canvas.drawArc(rectF, 0, 270, false, bgPaint);
-        canvas.restore();
-        canvas.save();
-        canvas.rotate(-90, centerX, centerY);
+//        canvas.restore();
+//        canvas.save();
+//        canvas.rotate(-90, centerX, centerY);
         for (int i = 0; i < 60; i++) {
 
-            if (i < 23 || i > 37)
-                canvas.drawLine(dotStartX, centerY - 1, dotStartX + 1, centerY + 1, dashPaint);
+            //画小圆点
+            if (i <= 54)
+                canvas.drawLine(dotStartX, centerY - 1, dotStartX + 1, centerY + 1, dotPaint);
 
-            canvas.drawArc(rectF, 0.2f, 5.6f, false, linePaint);
-
-            if (i == 14) {
-                canvas.drawLine(mWidth - padding - ringWidth, centerY - 2, mWidth, centerY + 2, currentLinePaint);
-//                canvas.drawArc(rectF, -0.2f, 0.2f, false, currentLinePaint);
-            }
-            canvas.rotate(6f, centerX, centerY);
+            canvas.drawArc(rectF, 0.2f, 4.6f, false, linePaint);
+            canvas.rotate(5f, centerX, centerY);
         }
+        canvas.restore();
+        drawCurrentScale(canvas);
+    }
+
+    /**
+     * 画当前刻度
+     *
+     * @return
+     */
+    private void drawCurrentScale(Canvas canvas) {
+        float currentTempDegree = (currentTemp - tempBegin) / deltaTemp;
+//        if (currentTempDegree < -225)
+//            currentTempDegree = -225;
+//        else if (currentTempDegree > 45)
+//            currentTempDegree = 45;
+        canvas.save();
+        canvas.rotate(-225 + currentTempDegree, centerX, centerY);
+        canvas.drawLine(mWidth - padding - ringWidth, centerY - 2, mWidth, centerY + 2, currentLinePaint);
         canvas.restore();
     }
 
+    /**
+     * 设置当前温度
+     *
+     * @param value 目标值
+     */
+    @MainThread
+    public void setCurrentTemp(int value) {
+        if (value < tempBegin || value > tempEnd) return;
+        currentTemp = value;
+        invalidate();
+    }
+
+    public int getCurrentTemp() {
+        return currentTemp;
+    }
 }
