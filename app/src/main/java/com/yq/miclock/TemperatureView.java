378d2388faa4b115a09abe10e0aa50767dd795fc
yinqi <251282197.com>
2019-07-29 15:59:48 +0800

    滑动处理

--- app/src/main/java/com/yq/miclock/TemperatureView.java
+++ app/src/main/java/com/yq/miclock/TemperatureView.java
@@ -93,6 +93,7 @@ public class TemperatureView extends View {
         centerX = getMeasuredWidth() / 2;
         centerY = getMeasuredHeight() / 2;
         ringWidth = (int) (scaleRatio * centerX);
+//        currentLinePaint.setStrokeWidth(ringWidth);
 
         bgPaint.setStrokeWidth(ringWidth);
         linePaint.setStrokeWidth(ringWidth + 4);
@@ -101,6 +102,9 @@ public class TemperatureView extends View {
 
 
     float downY = 0;
+    float currentX, lastX;
+    float currentY, lastY;
+    boolean continueDraw = true;//是否继续处理滑动事件
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
@@ -109,55 +113,69 @@ public class TemperatureView extends View {
             case MotionEvent.ACTION_DOWN:
                 downX = event.getX();
                 downY = event.getY();
-                System.out.println(downX);
-                break;
+                currentX = event.getX();
+                currentY = event.getY();
+                float curAngle = calcAngle(currentX, currentY);
+                System.out.println("angle start = " + angleToRotate);
+                if (angleToRotate - 10 < 0)
+                    continueDraw = curAngle >= angleToRotate - 10 + 360 || curAngle <= angleToRotate + 10;
+                else
+                    continueDraw = (curAngle <= angleToRotate + 10 && curAngle >= angleToRotate - 10);
+                return continueDraw;
             case MotionEvent.ACTION_MOVE:
-                float currentX = event.getX();
-                float currentY = event.getY();
-                calcAngle(currentX, currentY);
-                float diffX = currentX - downX;
-                System.out.println("diffX =  " + diffX);
-                setCurrentTempByDelta(calcDeltaTemp(diffX));
-                downX = currentX;
+                currentX = event.getX();
+                currentY = event.getY();
+                float currentAngle = calcAngle(currentX, currentY);
+                if ((isClockWise() && currentAngle > 60 && currentAngle <= 90) || (!isClockWise() && currentAngle >= 90 && currentAngle < 120)) {
+                    continueDraw = false;
+                } else if (currentAngle > 45 && currentAngle < 135) {
+//                    angleToRotate = lastAngle;
+//                    continueDraw = false;
+                } else {
+                    if (continueDraw) {
+                        angleToRotate = currentAngle;
+//                        lastAngle = angleToRotate;
+                        lastX = currentX;
+                        lastY = currentY;
+                        invalidate();
+                    }
+                }
+//                float diffX = currentX - downX;
+//                System.out.println("diffX =  " + diffX);
+//                setCurrentTempByDelta(calcDeltaTemp(diffX));
+//                downX = currentX;
+
                 break;
-//                return true;
             case MotionEvent.ACTION_UP:
 
                 break;
         }
-        return true;
+        return super.onTouchEvent(event);
     }
 
-    private void calcAngle(float currentX, float currentY) {
+    /**
+     * 是否是顺时针
+     *
+     * @return true:顺时针 false:逆时针
+     */
+    private boolean isClockWise() {
+        return (currentY > centerY && currentX < lastX) || (currentY < centerY && currentX > lastX);
+    }
+
+    private float calcAngle(float currentX, float currentY) {
         float deltaX = currentX - centerX;
         float deltaY = currentY - centerY;
         double sin = deltaY / (Math.sqrt(deltaX * deltaX + deltaY * deltaY));
         double angle = Math.toDegrees(Math.asin(sin));//角度
+
+        if (currentX >= centerX && currentY <= centerY)//第一象限
+            angle += 360;
+        else if (currentX <= centerX && currentY <= centerY)//第二象限
+            angle = -angle + 180;
+        else if (currentX < centerX && currentY > centerY)//第三象限
+            angle = 180 - angle;
         System.out.println("angle = " + angle);
-
-        if (currentX == centerX && currentY == centerY) return;
-
-//        if (currentX >= centerX && currentY <= centerY)//第一象限
-//            angleToRotate = angle;
-//        else if (currentX <= centerX && currentY <= centerY)//第二象限
-//            angleToRotate = -(angle + 180);
-//        else if (currentX < centerX && currentY > centerY)//第三象限
-//            angleToRotate = -angle - 180;
-//        else
-//            angleToRotate = angle;
-        if (currentX >= centerX && currentY <= centerY)
-            angleToRotate = angle;
-        else if (currentX >= centerX && currentY > centerY)
-            angleToRotate = angle - 360;
-        else
-            angleToRotate = -(angle + 180);
-        if (angleToRotate <= -225)
-            angleToRotate = -225;
-        else if (angleToRotate >= 45)
-            angleToRotate = 45;
-        else
-            lastAngle = angleToRotate;
-        System.out.println("angleToRotate = " + angleToRotate);
+        return (float) angle;
     }
 
     /**
@@ -187,6 +205,8 @@ public class TemperatureView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         drawBackground(canvas);
+//        canvas.drawLine(0, centerY - 2, mWidth, centerY + 2, currentLinePaint);
+//        canvas.drawLine(centerX - 2, 0, centerX + 2, mHeight, currentLinePaint);
 
     }
 
@@ -194,19 +214,13 @@ public class TemperatureView extends View {
     private void drawBackground(Canvas canvas) {
         RectF rectF = new RectF(0, 0, mWidth, mHeight);
         rectF.inset(ringWidth / 2 + padding, ringWidth / 2 + padding);
-//        RectF rectF = new RectF(ringWidth / 2 + padding, ringWidth / 2 + padding, getWidth() - ringWidth / 2 - padding, getHeight() - ringWidth / 2 - padding);
         canvas.save();
         canvas.rotate(-225, centerX, centerY);
-        canvas.drawArc(rectF, 0, 270, false, bgPaint);
-//        canvas.restore();
-//        canvas.save();
-//        canvas.rotate(-90, centerX, centerY);
+        canvas.drawArc(rectF, -0.2f, 270.2f, false, bgPaint);
         for (int i = 0; i < 60; i++) {
-
             //画小圆点
             if (i <= 54)
                 canvas.drawLine(dotStartX, centerY - 1, dotStartX + 1, centerY + 1, dotPaint);
-
             canvas.drawArc(rectF, 0.2f, 4.6f, false, linePaint);
             canvas.rotate(5f, centerX, centerY);
         }
@@ -221,24 +235,28 @@ public class TemperatureView extends View {
      */
     private void drawCurrentScale(Canvas canvas) {
         currentLinePaint.setTextSize(40);
-        canvas.drawText(currentTemp + "", centerX, centerY, currentLinePaint);
+//        canvas.drawText(currentTemp + "", centerX, centerY, currentLinePaint);
         float currentTempDegree = (currentTemp - tempBegin) / deltaTemp;
 //        if (currentTempDegree < -225)
 //            currentTempDegree = -225;
 //        else if (currentTempDegree > 45)
 //            currentTempDegree = 45;
         canvas.save();
-        canvas.rotate(-225 + currentTempDegree, centerX, centerY);
-        canvas.drawLine(mWidth - padding - ringWidth, centerY - 2, mWidth, centerY + 2, currentLinePaint);
+        System.out.println("angleToRotate draw = " + angleToRotate);
+        canvas.rotate((float) angleToRotate, centerX, centerY);
+        canvas.drawLine(mWidth - padding - ringWidth, centerY - 2, mWidth - padding / 2, centerY + 2, currentLinePaint);
+//        RectF rectF = new RectF(padding + ringWidth / 2, padding + ringWidth / 2, mWidth - padding - ringWidth / 2, mHeight - padding - ringWidth / 2);
+//        canvas.drawArc(rectF, -0.4f, 0.4f, false, currentLinePaint);
         canvas.restore();
-        double angle = angleToRotate;
-        if (lastAngle == -225 || lastAngle == 45)
-            angle = lastAngle;
-        canvas.save();
-        canvas.rotate((float) angle, centerX, centerY);
-        System.out.println("angleToRotate draw = " + angle);
-        canvas.drawLine(mWidth - padding - ringWidth, centerY - 2, mWidth, centerY + 2, curPaint);
-        canvas.restore();
+
+//        double angle = angleToRotate;
+//        if (lastAngle == 225 || lastAngle == 315)
+//            angle = lastAngle;
+//        canvas.save();
+//        canvas.rotate((float) angle, centerX, centerY);
+//        System.out.println("angleToRotate draw = " + angle);
+//        canvas.drawLine(mWidth - padding - ringWidth, centerY - 2, mWidth, centerY + 2, curPaint);
+//        canvas.restore();
     }
 
     /**
